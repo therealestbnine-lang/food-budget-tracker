e7a2e81cf0408e9f946b0d7726514a0e4c03d030
therealestbnine-lang <therealestbnine@gmail.com>
2026-09-04 13:04:57 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Minimal service worker: caches the app shell so it also opens offline
+// once installed. Bump CACHE_NAME if you ever change index.html and want
+// installed copies to pick up the update.
+const CACHE_NAME = "food-budget-tracker-v1";
+const SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
